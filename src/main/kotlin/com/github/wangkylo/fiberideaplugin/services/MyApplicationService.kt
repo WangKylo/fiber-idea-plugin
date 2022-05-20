98ba081f3a52ea29888516468fdb556d306be6af
GitHub Action <action@github.com>
2022-05-20 11:45:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangkylo/fiberideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wangkylo/fiberideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangkylo.fiberideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangkylo.fiberideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wangkylo/fiberideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangkylo/fiberideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangkylo.fiberideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangkylo.fiberideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
